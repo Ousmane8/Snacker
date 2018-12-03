ca9687f0bbd6144fe95b8a2d6250809e66fd5c78
Ousmane! <ousmane.sylla@ynov.com>
2018-12-03 15:27:52 +0100

    Initial Commit

---
+++ Snacker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Snacker
+//
+//  Created by Ousmane Sylla on 03/12/2018.
+//  Copyright © 2018 Ynov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
