75ab8515a021380e62657790abc70d0374302970
Ousmane! <ousmane.sylla@ynov.com>
2018-12-07 01:18:22 +0100

    avancé et maj
    mise en avant de la page Appicon et modification sur le main.storyboard

---
+++ Snacker/Page2ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  Page2ViewController.swift
+//  Snacker
+//
+//  Created by Ousmane Sylla on 06/12/2018.
+//  Copyright © 2018 Ynov. All rights reserved.
+//
+
+import UIKit
+
+class Page2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func Sortir(_ sender: UIButton) {
+        exit(0)
+    }
+    
+
+}
--- Snacker/ViewController.swift
+++ Snacker/ViewController.swift
@@ -40,6 +40,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
         return (true)
     }
-
+    
+    
 }
 
